b0e909fb20478ffba78515d49fd9efbcd8a45831
Juliana Montes <jmontes@jamcity.com>
2020-04-03 09:53:55 -0500

    Added day progress

---
+++ WorkFromHome/Assets/Scripts/TimeManagement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManagement : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private float dayProgress;
+    [SerializeField] private float daySpeed;
+
+    private bool inProgress = true;
+
+    private void Start()
+    {
+        slider.maxValue = 100;
+    }
+
+    private void Update()
+    {
+        if (inProgress)
+        {
+            dayProgress += Time.deltaTime * daySpeed;
+            if (dayProgress >= slider.maxValue)
+            {
+                dayProgress = slider.maxValue;
+                inProgress = false;
+            }
+            slider.value = dayProgress;
+        }
+    }
+}
