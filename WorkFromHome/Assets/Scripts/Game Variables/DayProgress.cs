2cc245f66c46f9cc32f0a1f0e25e3eb182be9eef
Juliana Montes <jmontes@jamcity.com>
2020-04-03 10:28:52 -0500

    Cleaned up the time progress bar logic

---
+++ WorkFromHome/Assets/Scripts/Game Variables/DayProgress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayProgress : MonoBehaviour
+{
+    [SerializeField] private float daySpeed;
+    [SerializeField] private float secondsPerDay;
+
+    private float secondsPassed;
+
+    public float SecondsPassed
+    {
+        get { return secondsPassed; }
+        private set { secondsPassed = value;  }
+    }
+
+    public float SecondsPerDay
+    {
+        get { return secondsPerDay; }
+        private set { secondsPerDay = value; }
+    }
+
+    private bool inProgress = true;
+
+    private void Update()
+    {
+        if (inProgress)
+        {
+            secondsPassed += Time.deltaTime * daySpeed;
+            if (secondsPassed >= SecondsPerDay)
+            {
+                secondsPassed = SecondsPerDay;
+                inProgress = false;
+            }
+        }
+    }
+}
---
+++ WorkFromHome/Assets/Scripts/UI/DaySecondsSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DaySecondsSlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private DayProgress dayProgress;
+
+    void Start()
+    {
+        slider.maxValue = dayProgress.SecondsPerDay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = dayProgress.SecondsPassed;
+    }
+}
