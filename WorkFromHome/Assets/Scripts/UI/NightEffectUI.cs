92c5d6aae5ce48d423523d35be20be57ffc2bfbd
Nicolas Trillos <ntrillos@jamcity.com>
2020-04-04 23:30:00 -0500

    Added nighttime effect when day is ending

---
+++ WorkFromHome/Assets/Scripts/UI/NightEffectUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NightEffectUI : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private DayProgress dayProgress;
+    [SerializeField] private float dayNightimePercentage;
+
+    private void Update()
+    {
+        if(dayProgress)
+        {
+            if (dayProgress.SecondsPassed / dayProgress.SecondsPerDay >= dayNightimePercentage)
+                canvasGroup.alpha = (dayProgress.SecondsPassed / dayProgress.SecondsPerDay - 0.6f) / (1f - dayNightimePercentage);
+            else
+                canvasGroup.alpha = 0f;
+        }
+        else
+        {
+            canvasGroup.alpha = 0f;
+        }
+    }
+}
