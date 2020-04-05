21adb1861add5fa52f9560fc35a3f62e676f0df2
Nicolas Trillos <ntrillos@jamcity.com>
2020-04-05 18:36:13 -0500

    Visual Computer and chair upgrades

--- WorkFromHome/Assets/Scripts/UI/ChairImage.cs
+++ WorkFromHome/Assets/Scripts/UI/ChairImage.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ChairImage : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer chair;
+    [SerializeField] private List<Sprite> chairs;
+
+    private int currentChair;
 
     void Start()
     {
-        
+        currentChair = 0;
+        chair.sprite = chairs[currentChair];
     }
 
     public void UpdateChair()
     {
-        Debug.Log("Updated Chair");
+        if (currentChair < chairs.Count - 1)
+        {
+            Debug.Log("Updated Chair");
+            currentChair++;
+            chair.sprite = chairs[currentChair];
+        }
     }
 }
--- WorkFromHome/Assets/Scripts/UI/ComputerImage.cs
+++ WorkFromHome/Assets/Scripts/UI/ComputerImage.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ComputerImage : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer computer;
+    [SerializeField] private List<Sprite>  computers;
+
+    private int currentComputer;
 
     void Start()
     {
-        
+        currentComputer = 0;
+        computer.sprite = computers[currentComputer];
     }
 
     public void UpdateComputer()
     {
-        Debug.Log("Updated Computer");
+        if (currentComputer < computers.Count - 1)
+        {
+            Debug.Log("Updated Computer");
+            currentComputer++;
+            computer.sprite = computers[currentComputer];
+        }
     }
 }
